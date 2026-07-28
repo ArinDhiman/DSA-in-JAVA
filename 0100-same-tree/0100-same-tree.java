f661c58b59470cacf05bc89bb57a8c5712dad5d5
Arin Dhiman <arindhiman.in@gmail.com>
2026-07-28 22:25:44 +0600

    Time: 0 ms (100%), Space: 42.9 MB (34.04%) - LeetHub

--- 0100-same-tree/0100-same-tree.java
+++ 0100-same-tree/0100-same-tree.java
@@ -24,9 +24,13 @@ class Solution {
         if(p.val!=q.val){
             return false;
         }
-
-        return isSameTree(p.left,q.left) &&
-        isSameTree(p.right,q.right);
+        if(isSameTree(p.left,q.left)==false){
+            return false;
+        }
+        if(isSameTree(p.right,q.right)==false){
+            return false;
+        }
+        return true;
         
     }
 }
